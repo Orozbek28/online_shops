470f1b2d5ae37b6952268275e753666986a918d1
Orozbek28 <gulmuradovorozbek297@gmail.com>
2023-05-08 12:18:57 +0600

    ddddd

--- items/urls.py
+++ items/urls.py
@@ -11,9 +11,7 @@ router.register('order', views.OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('register/user/', views.UserRegisterView.as_view(), name='register_user'),
-    # path('product/search/<str:name>', views.phone_detail, name='phone_detail'),
-    # path('category/search/<str:name>', views.category_detail, name='category_detail'),
-    # path('order/search/<str:name>', views.order_detail, name='order_detail'),
-    # path('product/<int:product_id>/', views.product_detail)
+    path('product/search/<str:name>', views.phone_detail, name='phone_detail'),
+    path('category/search/<str:name>', views.category_detail, name='category_detail'),
+    path('order/search/<str:name>', views.order_detail, name='order_detail'),
 ]
